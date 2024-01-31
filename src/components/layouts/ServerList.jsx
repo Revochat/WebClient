fc103c3fac1067884057417495bf677b5615d1fe
kouci747 <koceila.arhab@gmail.com>
2024-01-31 11:38:24 +0100

    serverList front WIP

--- src/components/layouts/ServerList.jsx
+++ src/components/layouts/ServerList.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 
+//liste de noms de servers
+const servers = [
+  {
+    id: 1,
+    name: 'Server 1',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    name: 'Server 2',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 3,
+    name: 'Server 3',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 4,
+    name: 'Server 4',
+    avatar: 'https://picsum.photos/200/300',
+  },
+];
+
 const ServerList = () => {
-    return (
-        <div className='h-full w-32 bg-red-300'>
-            <h1>ServerList</h1>
-        </div>
-    );
-}
+  return (
+    <div className="h-full w-28 bg-red-300">
+      <h1>ServerList</h1>
+      <ul>
+        {servers.map(server => (
+          <li key={server.id} className="group  transition-all duration-500 ">
+            <div className="relative flex items-center mb-5 ml-5">
+              <img
+                className="rounded-full overflow-hidden w-12 h-12  hover:animate-pulse transform hover:scale-125 focus:scale-125 active:scale-125 transition-transform"
+                src={server.avatar}
+                alt={server.name}
+              />
+              <div className="absolute left-12 opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-black text-white px-4 py-2 rounded-md whitespace-no-wrap w-32">
+                {server.name}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ServerList;
--- src/context/context.js
+++ src/context/context.js
@@ -1,74 +1,69 @@
 'use client';
-import { createContext, useState, useEffect } from "react";
-import { Revochat } from '@revochat/revochat-client';
-import EventList from "./EventList";
-
+import {createContext, useState, useEffect} from 'react';
+import {Revochat} from '@revochat/revochat-client';
+import EventList from './EventList';
 
 export const RevochatContext = createContext();
 
+export const RevochatProvider = ({children}) => {
+  const revochatClient = new Revochat.Client({
+    url: 'ws://localhost:4001',
+    debug: false,
+  });
 
-    export const RevochatProvider = ({children}) => {
-        const revochatClient = new Revochat.Client({
-            url: "ws://localhost:3001",
-            debug: false,
-        })
+  const [currentUser, setCurrentUser] = useState({});
+  const [revoLogin, setRevoLogin] = useState(false);
+  const [selectedChannel, setSelectedChannel] = useState({});
 
-        const [currentUser, setCurrentUser] = useState({});
-        const [revoLogin, setRevoLogin] = useState(false);
-        const [selectedChannel, setSelectedChannel] = useState({})
+  useEffect(() => {
+    if (!currentUser.user_id) initRevochat();
+  }, [currentUser]);
 
-        useEffect(() => {
-            if(!currentUser.user_id) initRevochat();
-        }, [currentUser])
+  const initRevochat = () => {
+    try {
+      logUser();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const logUser = () => {
+    try {
+      const jwt = localStorage.getItem('token');
+      console.log('jwt', jwt);
+      revochatClient.login(jwt);
 
-        const initRevochat = () => {
-            try {
-                logUser();
-            } catch (error) {
-                console.log(error)
-            }
-        }
+      revochatClient.on(EventList.User.Connect, user => {
+        if (user.error) return console.log(user.error);
+        console.log(user);
+        setCurrentUser(user);
+        localStorage.setItem('user', JSON.stringify(user));
+        !selectedChannel && setSelectedChannel(user.channels[0]);
+        setRevoLogin(true);
+        console.log(
+          'Connected as ' + user.username + ' (' + user.user_id + ')',
+        );
+        console.log('You have ' + user.friends.length + ' friends');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        const logUser = () => {
-            try{
-            const jwt = localStorage.getItem("token")
-                console.log('jwt', jwt)
-                revochatClient.login(jwt)
-            
-                revochatClient.on(EventList.User.Connect, (user) => {
-                    if(user.error) return console.log(user.error)
-                    console.log(user)
-                    setCurrentUser(user)
-                    localStorage.setItem('user', JSON.stringify(user))
-                    !selectedChannel && setSelectedChannel(user.channels[0])
-                    setRevoLogin(true)
-                    console.log("Connected as " + user.username +  " (" + user.user_id + ")")  
-                    console.log("You have " + user.friends.length + " friends")
-                })
-            }
-            catch(error){
-                console.log(error)
-            }
-        }
-         
-    
-    return (
-        <RevochatContext.Provider 
-            value={{
-                revochatClient,
-                logUser,
-                currentUser,
-                setCurrentUser,
-                revoLogin,
-                selectedChannel,
-                setSelectedChannel,
-
-            }}>
-            {children} 
-        </RevochatContext.Provider>
-    )
-}
+  return (
+    <RevochatContext.Provider
+      value={{
+        revochatClient,
+        logUser,
+        currentUser,
+        setCurrentUser,
+        revoLogin,
+        selectedChannel,
+        setSelectedChannel,
+      }}>
+      {children}
+    </RevochatContext.Provider>
+  );
+};
 
 export default RevochatProvider;
-
