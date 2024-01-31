bd8005bcedba069bdf3e2765629ec31c831f964d
ThomasJanko <thomas78125@gmail.com>
2024-01-31 12:24:37 +0100

    update: addFriend

--- src/components/AddFriend.jsx
+++ src/components/AddFriend.jsx
@@ -18,28 +18,47 @@ const AddFriend = () => {
         })
     }, [revoLogin])
 
+    useEffect(() => {
+        console.log('addFriend()')
+        if(!client) return;
+
+        client.on(EventList.User.AddFriend, (result) => {
+            console.log('add.friend: ', result)
+            if (result.error) {
+                console.error("Error:", result.error)
+                alert(result.error)
+                return;
+            };
+            setFriendID("")
+        })
+    }, [client]); 
+
 
     const handleChange = (e) => {
         setFriendID(e.target.value);
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            addFriend()
+        }
+    }
+
     const addFriend = () => {
         console.log('add friend ...')
        try{
-        client.user.addFriend({ username: friendID })
+            client.user.addFriend({ username: friendID })
        }
        catch(err){
            console.log('error: ', err)
-           alert(err)
        }
-        // client.emit(EventList.User.AddFriend, { username: friendID })
         console.log('friend request send')
     }
 
     return (
         <div className='flex gap-2 items-center'>
-            <input type="text" className='rounded-md outline-none px-2 py-1 h-fit' value={friendID} placeholder="Friend ID" onChange={handleChange} />
-            <button onClick={addFriend}> <BsFillSendPlusFill className='hover:text-primary hover:scale-110 transition-all' size={26} /> </button>
+            <input onKeyDown={handleKeyDown} type="text" className='rounded-md outline-none px-2 py-1 h-fit' value={friendID} placeholder="Friend ID" onChange={handleChange} />
+            <button onClick={addFriend} > <BsFillSendPlusFill className='hover:text-primary hover:scale-110 transition-all' size={26} /> </button>
         </div>
     );
 }
