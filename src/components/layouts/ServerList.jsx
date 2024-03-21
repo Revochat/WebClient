b54471770803d34f68472a6823257df0d3e85d38
ThomasJanko <thomas78125@gmail.com>
2024-03-21 10:49:26 +0100

    ServerList style and fix

--- src/components/layouts/ServerList.jsx
+++ src/components/layouts/ServerList.jsx
@@ -1,20 +1,22 @@
+import { LogOut, PlusSquare } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
-import { FaPlus } from "react-icons/fa";
 
-// Bouton ajouter un serveur
-const addButton = [
-  {
-    name: 'add server',
-    avatar: '',
-  },
-];
+
+const revochatServer = {
+  id: 0,
+  name: 'Revochat',
+  avatar: '/logo.svg',
+};
+
+
 
 // Liste de noms de serveurs
 const servers = [
   {
     id: 1,
     name: 'Server 1',
-    avatar: 'https://picsum.photos/200/300',
+    avatar: '/X.svg',
   },
   {
     id: 2,
@@ -24,51 +26,121 @@ const servers = [
   {
     id: 3,
     name: 'Server 3',
-    avatar: 'https://picsum.photos/200/300',
+    avatar: 'facebook.svg',
   },
   {
     id: 4,
     name: 'Server 4',
+    avatar: '/google.svg',
+  },
+  {
+    id: 5,
+    name: 'Server 5',
+    avatar: '/attachment.svg',
+  },
+  {
+    id: 6,
+    name: 'Server 6',
     avatar: 'https://picsum.photos/200/300',
   },
+  {
+    id: 7,
+    name: 'Server 7',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 8,
+    name: 'Server 8',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 9,
+    name: 'Server 9',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 10,
+    name: 'Server 10',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 11,
+    name: 'Server 11',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 12,
+    name: 'Server 12',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 13,
+    name: 'Server 13',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 14,
+    name: 'Server 14',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 15,
+    name: 'Server 15',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 16,
+    name: 'Server 16',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 17,
+    name: 'Server 17',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 18,
+    name: 'Server 18',
+    avatar: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 19,
+    name: 'Server 19',
+    avatar: 'https://picsum.photos/200/300',
+  },
+ 
 ];
 
 const ServerList = () => {
   const [activeServer, setActiveServer] = useState(null);
+  const router = useRouter();
 
   const handleServerClick = (serverId) => {
     console.log('serverId', serverId);
     setActiveServer(serverId);
   };
 
-  return (
-    <div className="h-full w-20 bg-[#0E0E15]">
-      {/* <h1>ServerList</h1> */}
-      <ul className='mt-4'>
-        {servers?.map((server) => (
-          <ServerItem key={server.id} server={server} handleServerClick={handleServerClick} activeServer={activeServer} />
-        ))}
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    router.push('/auth/login')
+  };
 
-        <div className='mt-8'>
-        {addButton.map((button, index) => (
-          <li key={index} className="group transition-all duration-500">
-            <div className="relative flex items-center mb-5 ml-5">
-              <div
-                className={`relative flex items-center justify-center bg-[#1E1F25] text-white rounded-lg p-1 z-10 text-xs h-12 w-12 border-4 border-transparent cursor-pointer transition-all duration-300 hover:border-white active:ring active:ring-white active:ring-opacity-50 ${
-                  activeServer === 'addButton' ? 'border-4 border-white' : ''
-                }`}
-                onClick={() => handleServerClick('addButton')}
-              >
-                <FaPlus size={22} />
-              </div>
-              <div className="absolute left-12 opacity-0 transition-opacity duration-300 group-hover:opacity-100 bg-black text-white px-4 py-2 rounded-md whitespace-no-wrap w-32">
-                {button.name}
-              </div>
-            </div>
-          </li>
-        ))}
+  return (
+    <div className="h-screen overflow-hidden w-20 bg-[#0E0E15] items-center">
+      <div className='w-full h-full flex flex-col justify-between items-center'>
+        <div className='mt-4 max-h-[90%] w-full flex flex-col items-center'>
+          <ServerItem server={revochatServer} handleServerClick={handleServerClick} activeServer={activeServer} />
+          <div className='mt-14 pt-2 max-h-[72%] overflow-auto scrollbar-hidden w-full flex flex-col gap-3 items-center'>
+            {servers.map((server) => (
+              <ServerItem server={server} handleServerClick={handleServerClick} activeServer={activeServer} />
+            ))}
+          </div>
+            <PlusSquare size={54} className='text-white cursor-pointer mt-2 hover:scale-105 transition-all' />
         </div>
-      </ul>
+        <div className='text-white mb-8'>
+            <LogOut size={30} className='text-white cursor-pointer' onClick={handleLogout} />
+        </div>
+      </div>
     </div>
   );
 };
@@ -79,10 +151,10 @@ const ServerItem = ({ server, handleServerClick, activeServer }) => {
   const [ isHovered, setIsHovered ] = useState(false)
 
   return (
-    <li key={server.id} className="transition-all duration-500">
-      <div className="relative flex items-center mb-5 ml-5">
+    <li key={server.id} className="transition-all duration-500 list-none">
+      <div className="relative flex items-center">
         <div
-          className={`absolute right-2 bottom-0 flex items-center justify-center bg-red-500 text-white rounded-full p-1 z-10 text-xs h-4 w-4 ${
+          className={`absolute right-0 bottom-0 flex items-center justify-center bg-red-500 text-white rounded-full p-1 z-10 text-xs h-4 w-4 ${
             activeServer === server.id ? 'border-2 border-white' : ''
           }`}
           onClick={() => handleServerClick(server.id)}
