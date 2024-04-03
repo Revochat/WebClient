3a3b5eee44c820fc46359ccdc74b0af15fd63d1e
ThomasJanko <thomas78125@gmail.com>
2024-04-03 16:06:21 +0200

    CDN URL env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     env: {
         REVO_API_URL: process.env.REVO_API_URL,
         REVO_CLIENT_URL: process.env.REVO_CLIENT_URL,
+        REVO_CDN_URL: process.env.REVO_CDN_URL,
     },
 
     images: {
