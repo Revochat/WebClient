68519e05405f11e3c8faef5eb4f76c9887aa8126
ThomasJanko <thomas78125@gmail.com>
2024-04-03 10:25:49 +0200

    addFiles

--- src/components/utils/InputMessage.jsx
+++ src/components/utils/InputMessage.jsx
@@ -4,18 +4,24 @@ import React, { useContext, useEffect, useState } from 'react';
 import { PiPaperPlaneRightFill } from "react-icons/pi";
 import Image from 'next/image';
 import { sendMessage } from '@/apis/sockets/messages';
+import { IoClose } from 'react-icons/io5';
 
 const InputMessage = () => {
     
     const { revochatClient, revoLogin, selectedChannel } = useContext(RevochatContext);
     const [message, setMessage] = useState('');
     const [client, setClient] = useState(null)
+    const [menuAttachment, setMenuAttachment] = useState(false)
 
     useEffect(() => {
         revochatClient.on(EventList.User.Connect, () => {
             setClient(revochatClient)
         })
     }, [revoLogin])
+
+    useEffect(() => {
+        setMenuAttachment(false)
+    }, [selectedChannel])
 
     
 
@@ -44,17 +50,60 @@ const InputMessage = () => {
         }
     }
 
-    const openAttachments = () => {}
-
     return (
-        <div className='px-4 flex items-center gap-3 w-full'>
+        
+        
+        <div className='px-4 flex items-center gap-3 w-full relative'>
+            {menuAttachment && <div className='absolute bottom-14 w-40 bg-primary rounded-sm p-2 text-white transition-all'>
+                <div className='relative w-full h-full ml-1 pb-1'>
+                    <span className='absolute top-0 right-0 cursor-pointer' onClick={()=> setMenuAttachment(false)}> <IoClose size={20} /> </span>
+                    <div className='pt-4 flex flex-col gap-2'>
+                        <div className='flex gap-3 items-center'>
+                            <div className='bg-white rounded-full h6 w-6 flex justify-center items-center p-1'>
+                                <Image
+                                    src="/camera.svg"
+                                    alt="camera"
+                                    width={26}
+                                    height={26}
+                                    className='cursor-pointer hover:scale-105 transition'
+                                />
+                            </div>
+                            <span>Camera</span>
+                        </div>
+                        <div className='flex gap-3 items-center'>
+                            <div className='bg-white rounded-full h6 w-6 flex justify-center items-center p-1'>
+                                <Image
+                                    src="/doc.svg"
+                                    alt="document"
+                                    width={26}
+                                    height={26}
+                                    className='cursor-pointer hover:scale-105 transition'
+                                />
+                            </div>
+                            <span>Documents</span>
+                        </div>
+                        <div className='flex gap-3 items-center'>
+                            <div className='bg-white rounded-full h6 w-6 flex justify-center items-center p-1'>
+                                <Image
+                                    src="/media.svg"
+                                    alt="media"
+                                    width={26}
+                                    height={26}
+                                    className='cursor-pointer hover:scale-105 transition'
+                                />
+                            </div>
+                            <span>Media</span>
+                        </div>
+                    </div>
+                </div>
+            </div>}
             <Image 
                 src="/attachment.svg"
                 alt="attachment"
                 width={26}
                 height={26}
                 className='cursor-pointer hover:scale-105 transition'
-                onClick={openAttachments}
+                onClick={() => setMenuAttachment(!menuAttachment)}
             />
             <div className='relative items-center h-10 flex w-full'>
                 <input className='w-full pl-6 py-2 px-2 rounded-xl outline-none' type="text" placeholder='Tapez le message...' value={message}
@@ -67,7 +116,7 @@ const InputMessage = () => {
                     width={20}
                     height={20}
                     className='absolute right-3 cursor-pointer hover:scale-105 transition'
-                    onClick={openAttachments}
+                    onClick={() => setMenuAttachment(!menuAttachment)}
                 />
             </div>
             <PiPaperPlaneRightFill
@@ -76,6 +125,7 @@ const InputMessage = () => {
                 onClick={Send}
              />
         </div>
+    
     );
 }
 
